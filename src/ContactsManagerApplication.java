388817d4f746064e164cadaf358ba3d82cd3d77c
kicknback <joshua.borreli@gmail.com>
2021-07-16 11:51:42 -0500

    Created init() and built file path

--- src/ContactsManagerApplication.java
+++ src/ContactsManagerApplication.java
@@ -1,11 +1,18 @@
+import java.nio.file.Path;
+
 public class ContactsManagerApplication {
 
     public static void main(String[] args) {
 
+        init();
 
 
     }
 
+    public static void init() {
+        Path filePath = PathClass.getPath( "src","contacts.txt").toAbsolutePath();
 
+
+    }
 
 }
